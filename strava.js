327fb6b10324a93e5bd5a81062d315e154d5d75f
florian <florian.carriere@gmail.com>
2022-01-23 22:16:37 +0100

    test 2

--- strava.js
+++ strava.js
@@ -11,20 +11,6 @@ function getActivities(res){
     .catch(function (err) {console.log('error: ' + err);})
 }
 
-function appendData(data) {
-  var mainContainer = document.getElementById("main");
-  for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
-    div.innerHTML = 'ID: ' + data[i].id
-      + '  // Date: ' + data[i].start_date
-      + '  // Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
-      + '  // Time: ' + Math.round(data[i].moving_time/60 * 100) / 100 + "mn"
-      + '  // Avg speed: ' + Math.round(1000 / 60 / data[i].average_speed * 100) / 100 + "mn/km";
-    mainContainer.appendChild(div);
-  }
-}
-
 function saveData(data) {
   fs.writeFile('strava_data.txt', data, 'utf-8', (err) => {
       console.log('File created')
@@ -32,11 +18,9 @@ function saveData(data) {
 }
 
 function reAuthorize(){
-
   var id;
   var secret;
   var token;
-
   try {
     const keys = fs.readFileSync('strava_keys.json', 'utf8')
     id = keys.client_id;
