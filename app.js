12670f8185b93c11179590688099189b21e6f125
florian <florian.carriere@gmail.com>
2022-01-23 18:40:26 +0100

    test path

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+const path = require('path')
+
+products_path = '/data/products/products.json'
+products_file = path.basename(products_path)
+
+console.log(products_file) // products.json
