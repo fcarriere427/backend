94944fc4fbe2428cc840c34cd102dcb6d6dba8ed
florian <florian.carriere@gmail.com>
2022-01-24 22:16:27 +0100

    res.id

--- t.js
+++ t.js
@@ -49,10 +49,9 @@ function reAuthorize(){
 function getActivities(token){
   // appelle API strava avec l'access token qu'on vient de renouveller
   const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${token}`;
-  console.log('URL for get request:',activities_link);
   https.get(activities_link, (res) => {
     console.log('statusCode:',res.statusCode);
-    console.log('res:',res);
+    console.log('res.id:',res.id);
 
 //    .then(function (response) {return response.json();})
 //    .then(function (data) {appendData(data);})
