e099c7d3a4695855c9f85aa702b07d452d95032c
florian <florian.carriere@gmail.com>
2022-01-28 22:51:02 +0100

    update server

--- server.js
+++ server.js
@@ -2,14 +2,38 @@
 // Serveur : gère les requêtes du front
 ////////////////
 
+const fs = require('fs').promises;
+
 const express = require('express')
 const app = express()
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => res.json({ Hello: 'World!' }));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/readFile', async (req, res) => {
+  try{
+    // When an Error is thrown in the try block the rest
+    // of the block is not executed, execution continues
+    // in the catch bloclk
+  	const fileContent = await fs.readFile('helloworld.txt','utf8');
+  	res.json({
+      fileContent,
+      error: false
+    });
+  } catch (err) {
+    // In the catch block we receive the error as an argument
+    // set response status to 500, to denote a internal error
+    res.status(500);
+    res.json({
+      fileContent: null,
+      error: true,
+      errorMsg: err,
+    })
+  }
+});
+
+app.listen(
+  port,
+  () => console.log(`app listening at http://localhost:${port}`)
+);
