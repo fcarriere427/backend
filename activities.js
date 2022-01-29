9def6445b3cd0c8b146b38873e2390440aafb4b4
florian <florian.carriere@gmail.com>
2022-01-29 09:28:25 +0100

    update activities route

--- activities.js
+++ activities.js
@@ -9,7 +9,9 @@ router.use(function timeLog(req, res, next) {
 // define the home page route
 router.get('/', function(req, res) {
   res.send('Ici, on va appeler les données Strava');
-
+  res.status.json({
+    data: "données strava :-)";
+  });
 });
 // define the about route
 router.get('/about', function(req, res) {
