e198162de209c27f3acaf6f01dc617c5ff042437
florian <florian.carriere@gmail.com>
2022-01-29 22:37:57 +0100

    insert prom dans le router

--- activities.js
+++ activities.js
@@ -14,8 +14,11 @@ router.use(function timeLog(req, res, next) {
 // define the home page route
 router.get('/', function(req, res) {
     console.log("router - 0 - avant l'appel à newProm")
-    newProm();
-    console.log("router - 1 - après l'appel à newProm")
+    return new Promise((resolve, reject) => {
+      newProm();
+      console.log("router - 1 - à l'intérieur de la promise du router")
+    }
+    console.log("router - 2 - après l'appel à newProm")
 });
 
 module.exports = router;
