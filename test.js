ea6f96ec9d0aeceeb39f0c6084476de97859f391
florian <florian.carriere@gmail.com>
2022-01-29 22:56:48 +0100

    test promise basique

---
+++ test.js
@@ -0,0 +1,14 @@
+var prom = new Promise((resolve,reject) => {
+  console.log("createur de la promise");
+  resolve("retour créateur");
+});
+
+prom
+.then((value)) => {
+  console.log("retour du createur = " + value);
+  resolve("retourne du 1er then");
+});
+.then((value)) => {
+  console.log("retour du 1er then = " + value);
+  resolve("retourne du 2eme then");
+});
