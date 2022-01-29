2a38bd037384d6e8bc8003a7a3c98f90bf216d07
florian <florian.carriere@gmail.com>
2022-01-29 22:59:28 +0100

    correct

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
-var prom = new Promise((resolve,reject) => {
-  console.log("createur de la promise");
-  resolve("retour créateur");
-});
+function test() {
+  var prom = new Promise((resolve,reject) => {
+    console.log("createur de la promise");
+    resolve("retour créateur");
+  });
 
-prom.then((value)) => {
-  console.log("retour du createur = " + value);
-  resolve("retourne du 1er then");
-});
-.then((value)) => {
-  console.log("retour du 1er then = " + value);
-  resolve("retourne du 2eme then");
-});
+  prom.then((value)) => {
+    console.log("retour du createur = " + value);
+    resolve("retourne du 1er then");
+  });
+  .then((value)) => {
+    console.log("retour du 1er then = " + value);
+    resolve("retourne du 2eme then");
+  });
+}
+
+test();
