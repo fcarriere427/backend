3d0619dde51fb93c74b633351ca39ba9b0df7e4c
florian <florian.carriere@gmail.com>
2022-01-29 23:05:26 +0100

    simpler

--- test.js
+++ test.js
@@ -8,10 +8,6 @@ function test() {
     console.log("retour du createur = " + value);
     resolve("retourne du 1er then");
   });
-  .then((value) => {
-    console.log("retour du 1er then = " + value);
-    resolve("retourne du 2eme then");
-  });
 }
 
 test()
