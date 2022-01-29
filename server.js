fdc08737e4f949da4cdd1fd3540dc0e62d42a54d
florian <florian.carriere@gmail.com>
2022-01-29 07:03:35 +0100

    correct

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Running on http://localhost:${port}");
+  console.log('Running on http://localhost:'+ port);
   }
 );
 
