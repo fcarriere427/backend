7d63ceab719fcfb33868ca8f778fe7a431c001fd
florian <florian.carriere@gmail.com>
2022-01-29 08:01:28 +0100

    simplify

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ var activities = require('./activities');
 
 app.use('/activities', activities);
 
-app.get('/', (req, res) => {
-  res.send('réponse de server.js');
-});
+// app.get('/', (req, res) => {
+//   res.send('réponse de server.js');
+// });
 
-app.get('/activities', (req, res) => {
-  res.send('ici il faudrait appeler la route activities');
-});
+// app.get('/activities', (req, res) => {
+//   res.send('ici il faudrait appeler la route activities');
+// });
 
 app.listen(port, () => {
   console.log('Running on http://localhost:'+ port);
