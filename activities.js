0a52b3bad03d2c2e63a3fa61145ac125edbba146
florian <florian.carriere@gmail.com>
2022-01-29 10:32:34 +0100

    modify

--- activities.js
+++ activities.js
@@ -13,28 +13,17 @@ router.use(function timeLog(req, res, next) {
 router.get('/', function(req, res) {
   var str = getActivities();
   res.status(200).json({
-    // ******* REPRENDRE ICI ************* //
-    // récuprer les vraies données Strava :-)
+    // récupérer les vraies données Strava :-)
     data: str
   });
   // res.send('Ici, on va appeler les données Strava');
-});
-// define the about route
-router.get('/about', function(req, res) {
-  res.send('About activities');
 });
 
 // Récupère les activités Strava
 function getActivities(){
   //lance le renouvellement de l'access token
-  const token = new Promise((res,rej) => {
-    res(reAuthorize());
-  })
-
-  // ******* REPRENDRE ICI ************* //
-  // que récupère-t-on dans token ?
-
-  token.then((value) => {
+  reAuthorize()
+  .then(result => {
     console.log("On va lancer getActivities avec token : " + token);
     // appelle API strava avec l'access token qu'on vient de renouveller
     const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${token}`;
@@ -58,49 +47,51 @@ function getActivities(){
 
 // Renouvelle le token d'access Strava
 function reAuthorize(){
-  console.log("on lance reAuthorize");
-  // Récupère les clés nécessaire dans le fichier (dispo en local seulement)
-  var data = fs.readFileSync('./strava_keys.json'), myObj;
-  try {
-    myObj = JSON.parse(data);
-    var id = myObj.id;
-    var secret = myObj.secret;
-    var token = myObj.token;
-  } catch (err) {
-    console.log('There has been an error reading the keys file :-(')
-    console.error(err)
-  }
-// fait la requête POST de renouvellement sur l'API strava
-  var body = JSON.stringify({
-    client_id: id,
-    client_secret: secret,
-    refresh_token: token,
-    grant_type: 'refresh_token'
-  })
-  var options = {
-    hostname: 'www.strava.com',
-    port: 443,
-    path: '/oauth/token',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': body.length
+  return new Promise((successCallback, failureCallback) => {
+    console.log("on lancela promesse de reAuthorize");
+    // Récupère les clés nécessaire dans le fichier (dispo en local seulement)
+    var data = fs.readFileSync('./strava_keys.json'), myObj;
+    try {
+      myObj = JSON.parse(data);
+      var id = myObj.id;
+      var secret = myObj.secret;
+      var token = myObj.token;
+    } catch (err) {
+      console.log('There has been an error reading the keys file :-(')
+      console.error(err)
     }
-  }
-  // lance la requête, et enchaine sur getActivities
-  var req = https.request(options, (res) => {
-    //*** A revoir : normalement, il faudrait attendre d'avoir tout reçu, donc res.on('end')...
-    res.on('data', (chunk) => {
-        const data = JSON.parse(chunk);
-        const token = data.access_token;
-        console.log("reAuthorize a récupéré : " + token);
-      });
+  // fait la requête POST de renouvellement sur l'API strava
+    var body = JSON.stringify({
+      client_id: id,
+      client_secret: secret,
+      refresh_token: token,
+      grant_type: 'refresh_token'
     })
-  req.on('error',(e) => {
-    console.error(e)
-  });
-  req.write(body);
-  req.end();
+    var options = {
+      hostname: 'www.strava.com',
+      port: 443,
+      path: '/oauth/token',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': body.length
+      }
+    }
+    // lance la requête, et enchaine sur getActivities
+    var req = https.request(options, (res) => {
+      //*** A revoir : normalement, il faudrait attendre d'avoir tout reçu, donc res.on('end')...
+      res.on('data', (chunk) => {
+          var data = JSON.parse(chunk);
+          var token = data.access_token;
+          console.log("reAuthorize a récupéré : " + token);
+        });
+      })
+    req.on('error',(e) => {
+      console.error(e)
+    });
+    req.write(body);
+    req.end();
+  }
 }
 
 module.exports = router;
