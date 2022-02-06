177a926bedf3a497c3eb67a2e064cef5d351b099
florian <florian.carriere@gmail.com>
2022-02-06 22:48:01 +0100

    add again ?

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 ////////////////
 // Serveur : gère les requêtes du front
 ////////////////
-
 const express = require('express')
+const app = express()
+const port = 3000
 
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://localhost:27017';
 const dbName = 'strava';
+let db;
 MongoClient.connect(url, function(err, client) {
   console.log("Connecté à MongoDB");
-  const db = client.db(dbName);
+  db = client.db(dbName);
   client.close();
 });
 
-const app = express()
-const port = 3000
 
 var activities = require('./activities');
 app.use('/activities', activities);
