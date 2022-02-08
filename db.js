6dd23cc06752612da7a0b25d3a6d9265fa665605
florian <florian.carriere@gmail.com>
2022-02-08 23:50:16 +0100

    remove db creation

--- db.js
+++ db.js
@@ -1,20 +1,5 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "boubou"
-});
-
-db.connect(function(err) {
-  if (err) throw err;
-  console.log('Connecté à la base de données');
-  db.query("CREATE DATABASE strava", function (err, result) {
-     if (err) throw err;
-     console.log("Base de données strava créée !");
-  });
-});
-
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
