26f4ee130ab9bc06d0de96ed45d0386096179f3a
florian <florian.carriere@gmail.com>
2022-02-08 23:48:26 +0100

    test db

--- db.js
+++ db.js
@@ -1,13 +1,32 @@
-const { Sequelize } = require('sequelize');
+const mysql = require('mysql');
 
-const sequelize = new Sequelize("strava", "root", "boubou", {
-    dialect: "mysql",
-    host: "localhost"
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "boubou"
 });
 
-try {
-   sequelize.authenticate();
-   console.log('Connecté à la base de données MySQL!');
- } catch (error) {
-   console.error('Impossible de se connecter, erreur suivante :', error);
- }
+db.connect(function(err) {
+  if (err) throw err;
+  console.log('Connecté à la base de données');
+  db.query("CREATE DATABASE strava", function (err, result) {
+     if (err) throw err;
+     console.log("Base de données strava créée !");
+  });
+});
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "boubou",
+  database : "strava"
+});
+
+con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connecté à la base de données MySQL!");
+   con.query("SELECT eleves.id as 'eleve_id', eleves.nom as 'eleve_nom', eleves.cours_id, cours.nom as 'cours_nom', cours.date as 'cours_date' FROM eleves JOIN cours on eleves.cours_id = cours.id", function (err, result) {
+       if (err) throw err;
+       console.log(result);
+     });
+ });
