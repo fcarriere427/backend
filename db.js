90892c4bfa3e2c0aa44c628d854106d60da36472
florian <florian.carriere@gmail.com>
2022-02-11 22:53:06 +0100

    cleaning

--- db.js
+++ db.js
@@ -14,8 +14,12 @@ const con = mysql.createConnection({
 con.connect(function(err) {
    if (err) throw err;
    console.log("Connecté à la base de données MySQL!");
-   con.query("SELECT eleves.id as 'eleve_id', eleves.nom as 'eleve_nom', eleves.cours_id, cours.nom as 'cours_nom', cours.date as 'cours_date' FROM eleves JOIN cours on eleves.cours_id = cours.id", function (err, result) {
-       if (err) throw err;
-       console.log(result);
-     });
  });
+
+
+
+//// exemple de requête
+// con.query("SELECT eleves.id as 'eleve_id', eleves.nom as 'eleve_nom', eleves.cours_id, cours.nom as 'cours_nom', cours.date as 'cours_date' FROM eleves JOIN cours on eleves.cours_id = cours.id", function (err, result) {
+//   if (err) throw err;
+//   console.log(result);
+// });
--- server.js
+++ server.js
@@ -2,16 +2,6 @@
 // Serveur : gère les requêtes du front
 ////////////////
 
-// const MongoClient = require('mongodb').MongoClient;
-// const url = 'mongodb://localhost:27017';
-// const dbName = 'strava';
-// let db;
-// MongoClient.connect(url, function(err, client) {
-//   console.log("Connecté à MongoDB");
-//   db = client.db(dbName);
-//   client.close();
-// });
-
 const express = require('express')
 const app = express()
 const port = 3000
