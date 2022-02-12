57be7b172f7ac8c2be3c02d29d5af77283a0188b
florian <florian.carriere@gmail.com>
2022-02-12 13:30:48 +0100

    log

--- storeData.js
+++ storeData.js
@@ -1,7 +1,19 @@
-var couch = require('./couchdb');
+//récupération des clés pour se connecter à couchDB
+console.log("Début de storeData.js...");
+const couchKeys = require('./keys/couchDB.json');
+var user = couchKeys.user;
+var pwd = couchKeys.password;
+var host = couchKeys.host;
+var port = couchKeys.port;
+var url = 'http://' + user + ':'' + pwd + '@' + host + ':' + port;
+console.log("url = " + url);
+const nano = require('nano')(url);
 
 couch.db.create('test2', function(err) {
   if (err) {
     console.error(err);
   }
 });
+
+
+// module.exports =
