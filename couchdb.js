acda6a094881893f8348839bd86417e806d5ef7a
florian <florian.carriere@gmail.com>
2022-02-12 22:57:28 +0100

    write to db

--- couchdb.js
+++
@@ -1,3 +0,0 @@
-var nano = require('nano');
-
-module.exports = nano(process.env.COUCHDB_URL || 'http://admin:Boubou13@192.168.1.35:5984');
--- storeData.js
+++ storeData.js
@@ -1,39 +1,39 @@
-//récupération des clés pour se connecter à couchDB
-console.log("Début de storeData.js...");
-const couchKeys = require('./keys/couchDB.json');
-var user = couchKeys.user;
-var pwd = couchKeys.password;
-var host = couchKeys.host;
-var port = couchKeys.port;
-var url = 'http://' + user + ':' + pwd + '@' + host + ':' + port;
-console.log("url = " + url);
-const nano = require('nano')(url);
+function StoreData(Data) {
 
-//
-const db = nano.use('strava');
+  // Récupération des clés pour se connecter à couchDB
+  console.log("Début de storeData.js...");
+  const couchKeys = require('./keys/couchDB.json');
+  var user = couchKeys.user;
+  var pwd = couchKeys.password;
+  var host = couchKeys.host;
+  var port = couchKeys.port;
+  var url = 'http://' + user + ':' + pwd + '@' + host + ':' + port;
+  console.log("url = " + url);
+  const nano = require('nano')(url);
 
-//var data = ''{ happy: true }, 'rabbit'';
-var o = {} // empty Object
-var key = 'Orientation Sensor';
-o[key] = []; // empty Array, which you can push() values into
+  // Ouverture de la BDD
+  const db = nano.use('strava');
+
+  // var o = {} // empty Object
+  // var key = 'Orientation Sensor';
+  // o[key] = []; // empty Array, which you can push() values into
+  // var data = {
+  //     sampleTime: '1450632410296',
+  //     data: '76.36731:3.4651554:0.5665419'
+  // };
+  // var data2 = {
+  //     sampleTime: '1450632410296',
+  //     data: '78.15431:0.5247617:-0.20050584'
+  // };
+  // o[key].push(data);
+  // o[key].push(data2);
+  // var doc = JSON.stringify(o);
+  console.log("data = " + data);
 
 
-var data = {
-    sampleTime: '1450632410296',
-    data: '76.36731:3.4651554:0.5665419'
-};
-var data2 = {
-    sampleTime: '1450632410296',
-    data: '78.15431:0.5247617:-0.20050584'
-};
-o[key].push(data);
-o[key].push(data2);
-var doc = JSON.stringify(o);
-console.log("doc = " + doc);
+  db.insert(data)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+}
 
-
-db.insert(o)
-.then((data) => console.log(data))
-.catch((err) => console.log(err))
-
-// module.exports =
+module.exports = storeData;
