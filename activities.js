5043f48a90a912f905e3986af630058d247eb082
florian <florian.carriere@gmail.com>
2022-02-13 08:18:25 +0100

    test

--- activities.js
+++ activities.js
@@ -64,7 +64,7 @@ function getActivities() {
       //const o = JSON.parse(data);
       //var myObj = JSON.parse(data);
       console.log('typeof Data dans getActivities = ' + typeof(data));
-      storeData(data);
+      //storeData(data);
     })
     .catch((err) => console.log(err))
     .then ((data) => resolve(data));
