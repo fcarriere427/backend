1daa379f1848f052d6e1b3f734d2851d84f361d6
florian <florian.carriere@gmail.com>
2022-02-20 22:20:49 +0100

    correct list_activities

--- list_activities.js
+++ list_activities.js
@@ -1,1 +1,7 @@
-console.log("on est dans list_activities !")
+function list_activities {
+  console.log("on est dans list_activities !");
+}
+
+list_activities()
+
+module.exports = list_activities;
