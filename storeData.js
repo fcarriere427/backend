f2c90f407b32289e733de9ae702d5659897335a6
florian <florian.carriere@gmail.com>
2022-02-20 10:48:46 +0100

    move var declaration

--- storeData.js
+++ storeData.js
@@ -1,21 +1,21 @@
 //*** Intégration des données dans la BDD
 
+const couchKeys = require('./keys/couchDB.json');
+var user = couchKeys.user;
+var pwd = couchKeys.password;
+var host = couchKeys.host;
+var port = couchKeys.port;
+var url = 'http://' + user + ':' + pwd + '@' + host + ':' + port;
+// Ouverture de la BDD
+const nano = require ('nano')(url);
+//console.log('nano = ' + JSON.stringify(nano));
+var stravaDb = nano.db.use('strava');
+
+var existingID = [];
+
 function storeData(data) {
   console.log("Début de storeData...");
   // Récupération des clés pour se connecter à couchDB
-  const couchKeys = require('./keys/couchDB.json');
-  var user = couchKeys.user;
-  var pwd = couchKeys.password;
-  var host = couchKeys.host;
-  var port = couchKeys.port;
-  var url = 'http://' + user + ':' + pwd + '@' + host + ':' + port;
-  // Ouverture de la BDD
-  const nano = require ('nano')(url);
-  //console.log('nano = ' + JSON.stringify(nano));
-  var stravaDb = nano.db.use('strava');
-
-  var existingID = [];
-
   // ********************
   insertDoc(data, stravaDb, function(){
     writeArray(stravaDb, function (){
