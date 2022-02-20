a593ac0c137b92b9a7e7d7d05f566f63a4792e19
florian <florian.carriere@gmail.com>
2022-02-20 12:05:02 +0100

    chain promises

--- storeData.js
+++ storeData.js
@@ -17,14 +17,14 @@ function storeData(data) {
   console.log("Début de storeData...");
   // Récupération des clés pour se connecter à couchDB
   // ********************
-  insertDoc(data, stravaDb, function(){
-    writeArray(stravaDb, function (){
-      console.log("Et voici le tableau des ID Strava : ");
-      for (var i = 0; i < existingID.length; i++) {
-        console.log("i = " + i + " =>" + existingID[i]);
-      }
-    });
-  });
+  insertDoc(data, stravaDd)
+  .then(writeArray(stravaDb))
+  .then(() => {
+    console.log("Et voici le tableau des ID Strava : ");
+    for (var i = 0; i < existingID.length; i++) {
+      console.log("i = " + i + " =>" + existingID[i]);
+    }
+  })
 }
 
 ///// REPRENDRE ICI : on récupère bien les docs en json, mais on ne sait pas extraire les valeurs qui nous  intéressent (par la clé "ID" de Strava)
@@ -34,40 +34,44 @@ function storeData(data) {
 //// AUTRE problème à traiter, moins urgent : il faut isoler le process de création initiale de la BDD...
 ///// ... sinon on ne sait pas s'il faut commencer par remplir la BDD ou le tableau des ID :-/
 
-function insertDoc(data, stravaDb, callback){
+function insertDoc(data, stravaDb){
   // Création d'un enregistrement pour chaque activité
-  console.log('Mise à jour de la BDD avec '+ data.length + ' éléments...');
-  for (let i = 0; i < data.length; i++) {
-    stravaDb.insert(data[i], function(error, http_body) {
-      if(!error) {
-        // Quand on est sur le dernier élément, alors seulement on appelle le callback !
-        if(i==data.length-1){
-          console.log('... OK, BDD mise à jour !');
-          callback();
-        };
-      } else {
-        console.log(error)
-      }
-    })
-  }
-}
-
-function writeArray(stravaDb, callback) {
-  console.log("Création du tableau avec les ID Strava...");
-  // pour chaque ligne de la BDD, on va écrire un élément dans le tableau existingID
-  stravaDb.list()
-  .then((body) => {
-    body.rows.forEach((item, i) => {
-      // console.log('Nouvel item : ');
-      // console.log(item);
-      getDoc(stravaDb, body, i)
-      .then(console.log('on a lu le doc et écrit existingID[' + i +'] = ' + existingID[i]));
-    });
+  return new Promise((resolve, reject) => {
+    console.log('Mise à jour de la BDD avec '+ data.length + ' éléments...');
+    for (let i = 0; i < data.length; i++) {
+      stravaDb.insert(data[i], function(error, http_body) {
+        if(!error) {
+          // Quand on est sur le dernier élément, alors seulement on appelle le callback !
+          if(i==data.length-1){
+            console.log('... OK, BDD mise à jour !');
+            resolve();
+          };
+        } else {
+          console.log(error)
+        }
+      })
+    }
   })
-  .then(() => callback());
 }
 
-function getDoc(stravaDb, body, i, callback){
+function writeArray(stravaDb) {
+  return new Promise((resolve, reject) => {
+    console.log("Création du tableau avec les ID Strava...");
+    // pour chaque ligne de la BDD, on va écrire un élément dans le tableau existingID
+    stravaDb.list()
+    .then((body) => {
+      body.rows.forEach((item, i) => {
+        // console.log('Nouvel item : ');
+        // console.log(item);
+        getDoc(stravaDb, body, i)
+        .then(console.log('on a lu le doc et écrit existingID[' + i +'] = ' + existingID[i]));
+      });
+    })
+    .then(resolve());
+  })
+}
+
+function getDoc(stravaDb, body, i){
   return new Promise((resolve,reject) => {
     console.log("Création d'une ligne dans le tableau des ID Strava...");
     stravaDb.get(body.rows[i].id)
