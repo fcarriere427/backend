cb312ffb89ba4990aa6552861f895bfeafc85f52
florian <florian.carriere@gmail.com>
2022-02-20 21:59:45 +0100

    update server

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use('/', function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+
 var list_activities = require('./list_activities');
-app.use('/list_activities', list_activities);
+app.get('/list_activities', list_activities);
 
 var fetch_activities = require('./fetch_activities');
-app.use('/fetch_activities', fetch_activities);
+app.get('/fetch_activities', fetch_activities);
 
 app.listen(port, () => {
   console.log('Serveur à l\'écoute sur localhost:'+ port);
