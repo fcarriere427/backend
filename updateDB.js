8d0b183ab92b4e8ea67b723694ee86cefcade544
florian <florian.carriere@gmail.com>
2022-02-20 15:19:51 +0100

    New !

--- updateDB.js
+++ updateDB.js
@@ -16,13 +16,18 @@ var existingID = [];
 
 function updateDB(data) {
   console.log('Début de updateDB...');
-  majBD(data, stravaDb)
-  .then(() => writeArray(stravaDb))
-  .then(() => {
-    for (let i = 0; i < existingID.length; i++) {
-      console.log('existingID[' + i + '] = ' + existingID[i]);
-    }
-  })
+  readID(stravaDb)
+  .then(() => insertNew());
+
+  // majBD(data, stravaDb)
+  // .then(() => writeArray(stravaDb))
+  // .then(() => {
+  //   for (let i = 0; i < existingID.length; i++) {
+  //     console.log('existingID[' + i + '] = ' + existingID[i]);
+  //   }
+  // })
+
+
 }
 
 ///// REPRENDRE ICI : on récupère bien les docs en json, mais on ne sait pas extraire les valeurs qui nous  intéressent (par la clé "ID" de Strava)
@@ -32,27 +37,31 @@ function updateDB(data) {
 //// AUTRE problème à traiter, moins urgent : il faut isoler le process de création initiale de la BDD...
 ///// ... sinon on ne sait pas s'il faut commencer par remplir la BDD ou le tableau des ID :-/
 
-function majBD(data, stravaDb){
+function insertNew(data, stravaDb){
   // Création d'un enregistrement pour chaque activité
   return new Promise((resolve, reject) => {
     console.log('Mise à jour de la BDD avec '+ data.length + ' éléments...');
     var count = 0;
     for (let i = 0; i < data.length; i++) {
-      stravaDb.insert(data[i], function(){
-        count = count + 1;
-        // Quand on est sur le dernier élément, alors seulement on appelle le callback !
-        if(count==data.length){
-          console.log('... OK, BDD mise à jour !');
-          resolve();
-        }
-      })
+      console.log('data[i].ID = ' + data[i].ID);
+      if(true) {
+        stravaDb.insert(data[i], function(){
+          count = count + 1;
+        })
+      }
+      // Quand on est sur le dernier élément, alors seulement on appelle le callback !
+      if(count==data.length){
+        console.log('... OK, BDD mise à jour !');
+        resolve();
+      }
+      }
     }
   })
 }
 
-function writeArray(stravaDb) {
+function readID(stravaDb) {
   return new Promise((resolve, reject) => {
-    console.log("Création du tableau avec les ID Strava...");
+    console.log("Création du tableau avec les ID Strava, à partir de la BDD existante...");
     var count = 0;
     // pour chaque ligne de la BDD, on va écrire un élément dans le tableau existingID
     stravaDb.list()
