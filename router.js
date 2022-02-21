c56e746490b70b36a4bdf1bb553f26d9ed185ace
florian <florian.carriere@gmail.com>
2022-02-21 23:55:09 +0100

    test

--- router.js
+++ router.js
@@ -7,16 +7,16 @@ const app = express()
 const port = 3000
 var router = express.Router();
 
+var list_activities = require('./list_activities');
+
 router.get('/strava_app/', function(req, res, next) {
-  console.log('/strava_app has been called')
+  console.log('/strava_app has been called');
+  list_activities;
 });
 
 router.get('/strava_app/list_activities', function(req, res, next) {
   console.log('/strava_app/list_activities has been called')
 });
-
-// var list_activities = require('./list_activities');
-// router.get('/list_activities', list_activities);
 
 // var fetch_activities = require('./fetch_activities');
 // router.get('/fetch_activities', fetch_activities);
