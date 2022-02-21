bebc044be7a198fb0f0f670c1bd9477de6bd84d7
florian <florian.carriere@gmail.com>
2022-02-21 23:46:37 +0100

    test

--- router.js
+++ router.js
@@ -9,12 +9,10 @@ var router = express.Router();
 
 router.get('/strava_app/', function(req, res, next) {
   console.log('/strava_app has been called')
-  next();
 });
 
 router.get('/strava_app/list_activities', function(req, res, next) {
   console.log('/strava_app/list_activities has been called')
-  next();
 });
 
 // var list_activities = require('./list_activities');
