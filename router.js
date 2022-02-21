21d6c7a1329d1271ebbaf5a0cc0932be9f8718ca
florian <florian.carriere@gmail.com>
2022-02-21 23:54:04 +0100

    test

--- router.js
+++ router.js
@@ -9,9 +9,6 @@ var router = express.Router();
 
 router.get('/strava_app/', function(req, res, next) {
   console.log('/strava_app has been called')
-  var div = document.createElement("div");
-  document.getElementById('div').innerHTML = "on a appelé /strava_app !!!";
-  mainContainer.appendChild(div);
 });
 
 router.get('/strava_app/list_activities', function(req, res, next) {
