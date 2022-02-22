f57c19695a48d674dc6643071051d12a11b7e5c0
florian <florian.carriere@gmail.com>
2022-02-22 21:54:36 +0100

    update

--- list_activities.js
+++ list_activities.js
@@ -1,8 +1,15 @@
-function list_activities () {
-  
-  console.log("on est dans list_activities !");
-}
+// Définition du router
+const express = require('express');
+var router = express.Router();
 
-list_activities()
+router.use(function timeLog(req, res, next) {
+  let newDate = new Date(Date.now());
+  console.log(`***** Appel de list_activities : ${newDate.toDateString()} ${newDate.toTimeString()}`);
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.status(200).send('on est dans list_activities !');
+});
 
 module.exports = list_activities;
