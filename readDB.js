556cd90668a1236057f214068d28d67d9f5b9ea9
florian <florian.carriere@gmail.com>
2022-02-24 22:28:34 +0100

    test

--- readDB.js
+++ readDB.js
@@ -16,7 +16,7 @@ function readDB() {
   return new Promise((resolve, reject) => {
     stravaDb.list()
     .then((body) => {
-      console.log('body = ' + body.stringify());
+      console.log('body.rows = ' + body.rows);
       resolve(body);
     })
   })
