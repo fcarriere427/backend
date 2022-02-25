474f9448bb27f7824f45ed267ac2c2568a8f0e3b
florian <florian.carriere@gmail.com>
2022-02-25 23:52:26 +0100

    test

--- readDB.js
+++ readDB.js
@@ -12,8 +12,6 @@ const nano = require ('nano')(url);
 //console.log('nano = ' + JSON.stringify(nano));
 var stravaDb = nano.db.use('strava');
 
-const createViewDB = require('./createViewDB');
-
 async function readDB() {
   stravaDb.get('_design/strava', { revs_info: true }, function(err, body) {
     if (err) {
@@ -29,5 +27,28 @@ async function readDB() {
     }
   });
 }
+
+function createViewDB() {
+  return new Promise((resolve, reject) => {
+    stravaDb.insert(
+    {"views":
+      {"activities_by_date":
+        {"map": function (doc) { emit (doc.start_date, doc); } }
+      }
+      // {"activities_by_distance":
+      //   {"map": function (doc) { emit (doc.distance, doc); } }
+      // }
+    },
+    '_design/strava',
+    function (error, response) {
+      if (!error){
+        console.log('OK, design created! response = ' + response + ' + error = ' + error);
+      } else {
+        console.log('OK, design created! response = ' + response + ' + error = ' + error);
+      }
+    })
+  })
+}
+
 
 module.exports = readDB;
