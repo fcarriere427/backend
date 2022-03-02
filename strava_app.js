c70d10ad35d619209fdeeb08cb7e1cbe8537ee53
florian <florian.carriere@gmail.com>
2022-03-02 17:12:57 +0100

    test webapp

--- strava_app.js
+++ strava_app.js
@@ -1,1 +1,6 @@
-module.exports = router;
+function main() {
+  var doc = document.implementation.createHTMLDocument();
+  doc.body.append('page principale de ma web app Strava !');
+}
+
+module.exports = main;
