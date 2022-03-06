28602e037e96007ee69349198c3f5e39c7b47d3a
florian <florian.carriere@gmail.com>
2022-03-06 23:15:27 +0100

    test

--- dbFunctions.js
+++ dbFunctions.js
@@ -17,12 +17,13 @@ var existingID = [];
 
 function readRec(id) {
   return new Promise((resolve, reject) => {
-    // ******* REPRENDRE ICI : comment récupérer le bon doc, avec l'ID Strava ?
-    // ******* COMMENT UTILISER L'INDEX qu'on a créé dans insert_new ?
     console.log('récupération du doc avec id = ' + id);
     var id_key = '"' + id + '"';
     console.log('id_key = ' + id_key);
-    stravaDb.view('strava', 'activities_by_id', {key: :id, include_docs: true}, function(err,body) {
+
+// ******* REPRENDRE ICI : comment passer l'id dans la key ???
+
+    stravaDb.view('strava', 'activities_by_id', { key: ('"'+ id + '"'), include_docs: true }, function(err,body) {
       if (!err) {
         console.log('on récupère : ' + JSON.stringify(body));
         resolve(body);
