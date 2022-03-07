b8639896ea3f1ee96741d51f4ce2e0fa80fa6e31
florian <florian.carriere@gmail.com>
2022-03-07 09:02:46 +0100

    test

--- dbFunctions.js
+++ dbFunctions.js
@@ -17,28 +17,19 @@ var existingID = [];
 
 function readRec(id) {
   return new Promise((resolve, reject) => {
-
-  console.log('récupération du doc avec id = ' + id);
-  stravaDb.view('strava', 'activities_by_id', {
+    console.log('récupération du doc avec id = ' + id);
+    stravaDb.view('strava', 'activities_by_id', {
     // ******* REPRENDRE ICI : comment passer l'id dans la key ???
-    key: [id],
-    include_docs: true,
-  }, function(err,body) {
+      startkey: [id],
+      include_docs: true,
+    }, function(err,body) {
       if (!err) {
         console.log('on récupère : ' + JSON.stringify(body));
         resolve(body);
       } else {
         console.log('error readDB = ' + err);
       }
-  });
-
-// ******* TEST AVEC QUERY : KO...
-    // const query = { selector: { id: { "$eq": id } } }
-    // stravaDb.find(query, function(err,body) {
-    //   console.log('on récupère : ' + JSON.stringify(body));
-    //   resolve(body);
-    // })
-
+    });
   })
 }
 
