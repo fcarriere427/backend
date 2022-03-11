87a1bca97d836ef61fdd331dc521d3557c9206e6
florian <florian.carriere@gmail.com>
2022-03-11 09:30:20 +0100

    test parseInt

--- dbFunctions.js
+++ dbFunctions.js
@@ -20,7 +20,8 @@ function readRec(id) {
     console.log('récupération du doc avec id = ' + id);
     // ******* REPRENDRE ICI : comment passer l'id dans la key ???
     // l'encapsulation entre crochets ou guillemets ne fonctionne pas
-    opts = ("[" + id + "]").split();
+    // opts = ("[" + id + "]").split();
+    const opts = parseInt(id);
     console.log('typeof opts : ' + typeof(opts));
     console.log('opts : ' + opts);
 
