b90cd4018bfaf0e05e5430ca7193fe42480a4809
florian <florian.carriere@gmail.com>
2022-03-13 00:42:49 +0100

    limit readDB to 2022

--- dbFunctions.js
+++ dbFunctions.js
@@ -32,7 +32,7 @@ function readRec(id) {
 
 function readDB() {
   return new Promise((resolve, reject) => {
-    stravaDb.view('strava', 'activities_by_year',{key: 2022, include_docs: true, descending: true}, function(err,body) {
+    stravaDb.view('strava', 'activities_by_year',{startkey: "2022-01-01T00:00:00Z", endkey: "2023-01-01T00:00:00Z",include_docs: true, descending: true}, function(err,body) {
       if (!err) {
         resolve(body.rows);
       } else {
@@ -137,13 +137,7 @@ function createViewDB() {
       "activities_by_distance":
       {"map": function (doc) { emit (doc.distance, null); } },
       "activities_by_id":
-      {"map": function (doc) { emit (doc.id, null); } },
-      "activities_by_year":
-      {"map": function (doc) {
-          let key = doc.start_date.getFullYear();
-          emit (key, null);
-        }
-      }
+      {"map": function (doc) { emit (doc.id, null); } }
     }
   },
   '_design/strava',
