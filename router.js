5b3eeb51cd7a3ea1cdd5cae7ba2c636fce5c778a
florian <florian.carriere@gmail.com>
2022-03-28 23:16:17 +0200

    test

--- router.js
+++ router.js
@@ -35,8 +35,7 @@ router.use(function timeLog(req, res, next) {
 
 router.get('/strava_old/testAPI', function(req, res) {
   console.log('appel de testAPI !');
-  res.json(427);
-  res.sendStatus(200);
+  res.send(427);
 });
 
 router.get('/strava_old/activity', function(req, res) {
