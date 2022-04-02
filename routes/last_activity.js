b3395ab62a6eb9889db9cba918157124f34c3abc
florian <florian.carriere@gmail.com>
2022-04-02 22:37:26 +0200

    update console log

--- routes/last_activity.js
+++ routes/last_activity.js
@@ -9,7 +9,7 @@ module.exports = {
               .then(date => { // au format "2022-04-02T07:43:20Z"
                 let newDate = new Date(date);
                 let date_str = newDate.toLocaleDateString('fr-FR') + ' at ' + newDate.toLocaleTimeString('fr-FR');
-                console.log("... date de la dernière activité renvoyée ! = " + date_str);
+                console.log("... Envoi de la date de la dernière activité (= " + date_str + ")");
                 res.status(200).json(date_str);
               })
             })
