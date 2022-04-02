9146d28b51e3103b96e523670a9fbe19b44708c8
florian <florian.carriere@gmail.com>
2022-04-02 22:03:43 +0200

    add last_activity

---
+++ routes/last_activity.js
@@ -0,0 +1,16 @@
+const dbFun = require('../dbFunctions');
+
+module.exports = {
+    path: "/strava/last_activity",
+    config: (router) => {
+        router
+            .get("/", (req, res) => {
+
+                console.log("... date de la dernière activité renvoyée !");
+                res.status(200).json(data);
+              })
+            })
+            .post("/", (req, res) => res.send("No POST here!"));
+        return router;
+    },
+};
