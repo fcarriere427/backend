ea53688f8ff7e20dac24f0e5e0a6c124ceb027ad
florian <florian.carriere@gmail.com>
2022-04-02 22:33:01 +0200

    clean date

--- routes/last_activity.js
+++ routes/last_activity.js
@@ -6,9 +6,11 @@ module.exports = {
         router
             .get("/", (req, res) => {
               dbFun.readLastActivityDate()
-              .then(date => {
-                console.log("... date de la dernière activité renvoyée ! = " + date);
-                res.status(200).json(date);
+              .then(date => { // au format "2022-04-02T07:43:20Z"
+                let newDate = new Date(date);
+                let date_str = newDate.toLocaleDateString('fr-FR') + ' at ' + newDate.toLocaleTimeString('fr-FR');
+                console.log("... date de la dernière activité renvoyée ! = " + date_str);
+                res.status(200).json(date_str);
               })
             })
             .post("/", (req, res) => res.send("No POST here!"));
