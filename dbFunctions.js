31aeaab0b9d2d543d1aba2d4893956fced328f7c
florian <florian.carriere@gmail.com>
2022-04-03 11:13:09 +0200

    add log

--- dbFunctions.js
+++ dbFunctions.js
@@ -225,6 +225,7 @@ function readAllYearDistance() {
     stravaDb.view('strava', 'group_by_month', {reduce: true, group_level: 1}, function(err,body) {
       if (!err) {
         body.rows.forEach(doc => {
+          console.log("year_distance[" + doc.key + "] = " + Math.round(doc.value/1000*10)/10);
           year_distance[doc.key] = Math.round(doc.value/1000*10)/10;
         });
         resolve(JSON.stringify(year_distance));
