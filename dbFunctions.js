79b7b0c05cd28060a2fa30c938ac672cab19b24c
florian <florian.carriere@gmail.com>
2022-04-03 13:48:07 +0200

    log

--- dbFunctions.js
+++ dbFunctions.js
@@ -227,6 +227,7 @@ function readAllYearDistance() {
         body.rows.forEach(doc => {
           if (doc != null) {
             console.log("year_distance[" + doc.key + "] = " + Math.round(doc.value/1000*10)/10);
+            console.log("year_distance.length = " + year_distance.length;
             year_distance[doc.key] = Math.round(doc.value/1000*10)/10;
           }
         });
