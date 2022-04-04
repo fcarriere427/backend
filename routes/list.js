be9ace20c440a4f6124fa0dc4da62d1ce4d74d84
florian <florian.carriere@gmail.com>
2022-04-04 23:31:50 +0200

    add list for test

---
+++ routes/list.js
@@ -0,0 +1,18 @@
+const dbFun = require('../dbFunctions');
+
+module.exports = {
+    path: "/strava/list",
+    config: (router) => {
+        router
+            .get("/", (req, res) => {
+              dbFun.readDB(req.query.year) // année pour filtrer
+              .then((data) => {
+                console.log("... liste des activités renvoyée, OK !");
+                res.setHeader('content-type', 'application/json');
+                res.status(200).send(data);
+              })
+            })
+            .post("/", (req, res) => res.send("No POST here!"));
+        return router;
+    },
+};
