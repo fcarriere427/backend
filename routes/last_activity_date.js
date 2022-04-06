282ac8e99e27751ab3513643e4b816b452ee96dc
florian <florian.carriere@gmail.com>
2022-04-06 22:24:30 +0200

    move to asycn

--- routes/last_activity_date.js
+++ routes/last_activity_date.js
@@ -5,15 +5,13 @@ module.exports = {
     config: (router) => {
         router
             .get("/", (req, res) => {
-              dbFun.readLastActivityDate()
-              .then(date => { // au format "2022-04-02T07:43:20Z"
-                let newDate = new Date(date);
-                let date_str = newDate.toLocaleDateString('fr-FR') + ' at ' + newDate.toLocaleTimeString('fr-FR');
-                console.log("... Envoi de la date de la dernière activité (= " + date_str + ")");
-                res.setHeader('content-type', 'application/json');
-                res.status(200).json({ last_activity_date: date_str});
+              let date = await dbFun.readLastActivityDate(); // au format "2022-04-02T07:43:20Z"
+              let newDate = new Date(date);
+              let date_str = newDate.toLocaleDateString('fr-FR') + ' at ' + newDate.toLocaleTimeString('fr-FR');
+              console.log("... Envoi de la date de la dernière activité (= " + date_str + ")");
+              res.setHeader('content-type', 'application/json');
+              res.status(200).json({ last_activity_date: date_str});
               })
-            })
             .post("/", (req, res) => res.send("No POST here!"));
         return router;
     },
