4a3ca1f13ebe19320ce400e416d464f55d5768ef
florian <florian.carriere@gmail.com>
2022-04-06 22:35:30 +0200

    remove seconds

--- routes/last_activity_date.js
+++ routes/last_activity_date.js
@@ -12,7 +12,7 @@ module.exports = {
                 let final = date_str.substring(0, date_str.length - 3); // on enlève les secondes
                 console.log("... Envoi de la date de la dernière activité (= " + final + ")");
                 res.setHeader('content-type', 'application/json');
-                res.status(200).json({ last_activity_date: date_str});
+                res.status(200).json({ last_activity_date: final});
                 })
               })
             .post("/", (req, res) => res.send("No POST here!"));
