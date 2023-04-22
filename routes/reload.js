91a4357c8627f1985f535eb104d3d3aefe101996
florian <florian.carriere@gmail.com>
2023-04-22 22:58:05 +0200

    More than 700 activities

--- routes/reload.js
+++ routes/reload.js
@@ -7,7 +7,7 @@ module.exports = {
         router
             .get("/", (req, res) => {
               // param de getActivities = nbPages --> ici 7(*100) car 615 activités Strava le 22/02/22 (cf. dashboard Strava) --> il faut mettre la centaine supérieure, pas plus !
-              var nbPages = 7;
+              var nbPages = 8;
               dbFun.renewDB()
               .then(() => strava.renewTokens())
               .then(() => strava.getActivities(nbPages))
